dfe0b92274d77834adcd61503ec6fa2008c1ec49
csboyty <csboyty@gmail.com>
2016-01-19 17:03:00 +0800

    init

---
+++ views/judge/menu.js
@@ -0,0 +1,5 @@
+document.write('<ul class="menu">'+
+    '<li><a href="works.html">作品列表</a></li>'+
+    '<li><a href="explain.html">评估说明</a></li>'+
+    '<li><a href="../../login.html">退出</a></li>'+
+    '</ul>');
---
+++ views/user/menu.js
@@ -0,0 +1,8 @@
+document.write('<ul class="menu">'+
+    '<li><a href="worksMgr.html">作品管理</a></li>'+
+    '<li><a href="../../login.html">上传作品</a></li> '+
+    '<li><a href="editInfo.html">资料修改</a></li>'+
+    '<li><a href="resetPwd.html">修改密码</a></li>'+
+    '<li><a href="explain.html">评估说明</a></li>'+
+    '<li><a href="../../login.html">退出</a></li>'+
+    '</ul>');
